171fb2f8a9c9a35338e1ff87a6f067974693da98
wamache <benardwamache@gmail.com>
2022-07-22 18:30:21 +0300

    second push

---
+++ src/main/java/com/schoolfeespayment/SchoolfeespaymentApplication.java
@@ -0,0 +1,13 @@
+package com.schoolfeespayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolfeespaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolfeespaymentApplication.class, args);
+	}
+
+}
